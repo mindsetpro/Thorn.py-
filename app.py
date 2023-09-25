4011aba4339ed903a3cf7a6b958484b0b568bfa0
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-09-25 12:54:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 import discord
 from discord.ext import commands
-from Thorn import Thorn
+import requests
+import json
 import os
 
-# Define your bot's intents (you may need to adjust these based on your bot's needs)
+# Define your bot's intents
 intents = discord.Intents.default()
-intents.typing = False  # Disable typing event
-intents.presences = False  # Disable presence update event
+intents.typing = False
+intents.presences = False
 
-# Retrieve the token from the GitHub secret
-BOT_TOKEN = os.environ.get("DISCORD_TOKEN")
-
-# Initialize the bot with the specified intents
+# Create an instance of the bot with the specified intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
@@ -21,13 +19,26 @@ async def on_ready():
 @bot.command()
 async def scrape(ctx, url):
     try:
-        # Scraping JSON data using Thorn module
-        json_data = Thorn.scrape_json(url)
-        
+        # Make a GET request to the URL and parse JSON data
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an exception for non-200 status codes
+        json_data = response.json()
+
+        # Convert the JSON data to a pretty-printed string for a code block
+        json_str = json.dumps(json_data, indent=4)
+        code_block = f"```json\n{json_str}\n```"
+
         # Sending the JSON data as a code block
-        await ctx.send(f"```json\n{json_data}\n```")
+        await ctx.send(code_block)
+    except requests.exceptions.RequestException as e:
+        await ctx.send(f"An error occurred while making the request: {e}")
+    except json.JSONDecodeError as e:
+        await ctx.send(f"Error decoding JSON data: {e}")
     except Exception as e:
-        await ctx.send(f"An error occurred: {e}")
+        await ctx.send(f"An unexpected error occurred: {e}")
+
+# Retrieve the bot token from the environment variable
+BOT_TOKEN = os.environ.get("DISCORD_TOKEN")
 
 # Run the bot with the retrieved token
 bot.run(BOT_TOKEN)
