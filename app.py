507dc131b86768b8b0c6b331e7092471d531aab3
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-09-25 14:20:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ intents.presences = False
 # Create an instance of the bot with the specified intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+# Directory for data storage
+DATA_DIR = "data"
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
@@ -28,14 +31,25 @@ async def scrape(ctx, url):
         json_str = json.dumps(json_data, indent=4)
         code_block = f"```json\n{json_str}\n```"
 
-        # Sending the JSON data as a code block
+        # Save the JSON data to a file
+        filename = os.path.join(DATA_DIR, "scraped_data.json")
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(json_data, file, indent=4)
+
+        # Send the JSON data as a code block and the file
         await ctx.send(code_block)
+        await ctx.send(file=discord.File(filename))
+
     except requests.exceptions.RequestException as e:
         await ctx.send(f"An error occurred while making the request: {e}")
     except json.JSONDecodeError as e:
         await ctx.send(f"Error decoding JSON data: {e}")
     except Exception as e:
         await ctx.send(f"An unexpected error occurred: {e}")
+
+# Create the data directory if it doesn't exist
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
 
 # Retrieve the bot token from the environment variable
 BOT_TOKEN = os.environ.get("DISCORD_TOKEN")
