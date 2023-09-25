28a147772f9c4900160c0bf134134c6234acb3d7
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-09-25 15:36:02 -0400

    Update thorn.py

--- thorn.py
+++ thorn.py
@@ -4,12 +4,11 @@ thorn.py - A powerful web scraper.
 
 import requests
 from bs4 import BeautifulSoup
-import json
+import json 
 import csv
 import logging
 from urllib.parse import urlparse, urlencode
 from cachetools import LRUCache
-from rotating_proxies.policies import BanDetectionPolicy
 from fake_useragent import UserAgent
 
 logger = logging.getLogger('thorn')
@@ -22,41 +21,24 @@ class Thorn:
         self.cache = LRUCache(maxsize=self.config['CACHE_SIZE'])
         self.session = requests.Session()
         self.user_agent = UserAgent()
-        self.proxies = None
-        if self.config['PROXY_URL']:
-            self.proxies = self.get_proxies(self.config['PROXY_URL'])
-        self.ban_policy = BanDetectionPolicy()
 
     def init_config(self):
         self.config['CACHE_SIZE'] = self.config.get('CACHE_SIZE', 500)
-        self.config['PROXY_URL'] = self.config.get('PROXY_URL', None)
-
-    def get_proxies(self, url):
-        """Fetch a list of proxy servers from a URL."""
-        resp = requests.get(url)
-        proxies = []
-        for proxy in resp.text.splitlines():
-            proxies.append({'http': proxy, 'https': proxy})
-        return proxies
 
     def scrape(self, url):
         """Scrape data from a URL."""
         
-        # Check cache
+        # Check cache 
         cache_key = hashlib.sha1(url.encode('utf-8')).hexdigest()
         if cache_key in self.cache:
-            logger.info(f'Cache hit for {url}')            
+            logger.info(f'Cache hit for {url}')
             return self.cache[cache_key]
 
-        # Rotate user agent and proxy
+        # Rotate user agent
         headers = {'User-Agent': self.user_agent.random}
-        if self.proxies:
-            proxy = next(self.proxies)
-            logger.info(f'Using proxy {proxy}')
-            self.session.proxies.update(proxy)
-
-        # Request page
-        logger.info(f'Scrape {url}')
+        
+        # Make request
+        logger.info(f'Scraping {url}')
         try:
             resp = self.session.get(url, headers=headers)
             resp.raise_for_status()
@@ -64,10 +46,10 @@ class Thorn:
             logger.error(f'Error scraping {url}: {e}')
             return None
 
-        # Parse response 
+        # Parse response
         page = BeautifulSoup(resp.text, 'html.parser')
         
-        # Scrape data
+        # Extract data
         data = self.extract_data(url, page)
         
         # Cache and return
@@ -75,29 +57,34 @@ class Thorn:
         return data
 
     def extract_data(self, url, page):
-        """Extract data from page based on url."""
+        """Extract data from page based on url"""
         if 'json' in url:
             return self.parse_json(page)
         elif 'csv' in url:
-            return self.parse_csv(page)        
+            return self.parse_csv(page)
         else:
             return self.parse_html(page)
 
     def parse_json(self, page):
-        """Extract JSON from page"""
-        ...
-
+        """Extract JSON data"""
+        script = page.find('script', {'type': 'application/json'})
+        if script:
+            data = json.loads(script.text)
+            return data
+    
     def parse_csv(self, page):
-        """Extract CSV from page"""
-        ...
-        
+        """Extract CSV data"""
+        text = page.find('pre').text
+        return list(csv.reader(text.splitlines()))
+
     def parse_html(self, page):
         """Extract raw HTML"""
-        ...
+        return page.prettify()
 
 if __name__ == '__main__':
     import sys
-    url = sys.argv[1] 
+    url = sys.argv[1]
+
     scraper = Thorn()
     data = scraper.scrape(url)
     print(data)
