8c92ee13d7bcb8c04a92ca49affcfc913d9100db
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-09-26 13:15:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
    name='thorn62.py',
-   version='1.2.9',
+   version='1.2.9',  
    packages=find_packages(),
-   install_requires=['requests']
+   install_requires=['requests'],
+
+   # Add PyPI API token
+   password=pypi-AgEIcHlwaS5vcmcCJDk3ODU5ZDFiLTc1ZGMtNGIwNS05MTBhLTY5OWY1OGRjYzgwOAACKlszLCJlN2RhZDZlMC0yZjYxLTRjNzctODVhYS1iN2I2MjAzYzI2NWEiXQAABiA1hJYvPDu_tw1Ro6GLKLuYf8jv7ax9WJfsBUL7yYA3Bw 
 )
