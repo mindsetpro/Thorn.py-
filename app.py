1aa0a86235d99faca8328535950740a73f2e8696
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-09-26 08:59:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 import os
-import requests
-from bs4 import BeautifulSoup
-
-from discord.ext import commands
-import thorn # Import the thorn scraper module
+import io
 import discord
+from discord.ext import commands
+import thorn
 
 # Bot setup
-intents = discord.Intents.default() 
+intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.command()
 async def scrape(ctx, url):
-    try:
-        # Use thorn scraper module
-        data = thorn.scrape_json(url)
-        
-        # Additional scraping with BeautifulSoup
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        titles = [h2.text for h2 in soup.find_all('h2')]
+    # Scrape with Thorn 
+    data = thorn.scrape_json(url)
+    
+    # Prettify JSON
+    json_text = thorn.pretty_json(data)
 
-        # Build response
-        content = f"**Scraped data:**\n{thorn.pretty_json(data)}\n\n**Page titles:**\n"
-        content += "\n".join(titles)
-        
-        await ctx.send(content)
-        
-    except Exception as e:
-        await ctx.send(f"Error: {e}")
-        
-BOT_TOKEN = os.environ.get("DISCORD_TOKEN")
-bot.run(BOT_TOKEN)
+    # Encode to bytes
+    bytes_data = json_text.encode('utf-8')
+
+    # Create in-memory file
+    mem_file = io.BytesIO(bytes_data) 
+
+    # Send as file 
+    await ctx.send(file=discord.File(mem_file, 'scraped.json'))
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is ready!')
+
+# Get Discord bot token from environment
+discord_token = os.environ.get('DISCORD_TOKEN') 
+
+# Run bot
+bot.run(discord_token)
