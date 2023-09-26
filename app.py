6d0ed8911c7e478e22dee0f6ee9669bdaf6cc184
RoseInjector <138173273+RoseInjector@users.noreply.github.com>
2023-09-26 08:52:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+
 from discord.ext import commands
-from thorn import Thorn
+import thorn # Import the thorn scraper module
 import discord
 
-intents = discord.Intents.default()
-intents.typing = False  # You can adjust these as needed
-intents.presences = False
-
+# Bot setup
+intents = discord.Intents.default() 
 bot = commands.Bot(command_prefix='!', intents=intents)
+
 @bot.command()
 async def scrape(ctx, url):
-
     try:
-        # Initialize Thorn scraper
-        scraper = Thorn()
+        # Use thorn scraper module
+        data = thorn.scrape_json(url)
         
-        # Use Thorn to scrape page
-        data = scraper.scrape(url)
-        
-        # Extract additional data with BeautifulSoup
+        # Additional scraping with BeautifulSoup
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         titles = [h2.text for h2 in soup.find_all('h2')]
-        
-        # Build response string
-        content = f"**Scraped data:**\n{data}\n\n**Page titles:**\n"
+
+        # Build response
+        content = f"**Scraped data:**\n{thorn.pretty_json(data)}\n\n**Page titles:**\n"
         content += "\n".join(titles)
         
         await ctx.send(content)
-
+        
     except Exception as e:
         await ctx.send(f"Error: {e}")
         
-BOT_TOKEN = os.environ.get("DISCORD_TOKEN")    
+BOT_TOKEN = os.environ.get("DISCORD_TOKEN")
 bot.run(BOT_TOKEN)
